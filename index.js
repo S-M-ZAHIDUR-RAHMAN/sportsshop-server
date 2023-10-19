5028413b0031169540e6c420bbfa338b7e41850a
S-M-ZAHIDUR-RAHMAN <smz.xahid@gmail.com>
2023-10-20 02:50:56 +0600

    post and get the cart data

--- index.js
+++ index.js
@@ -52,6 +52,12 @@ async function run() {
       const result = await cartCollection.insertOne(cart);
       res.send(result);
     })
+    app.get('/cart', async(req, res)=>{
+      const cursor = cartCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
 
 
     // Send a ping to confirm a successful connection
