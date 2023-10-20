be0705e89bb7dc29547688e7e8bf51695c1bfebe
S-M-ZAHIDUR-RAHMAN <smz.xahid@gmail.com>
2023-10-20 14:42:34 +0600

    done with delete function

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express ();
 const port = process.env.PORT || 5000;
 
@@ -55,6 +55,13 @@ async function run() {
     app.get('/cart', async(req, res)=>{
       const cursor = cartCollection.find();
       const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    app.delete('/cart/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: id}
+      const result = await cartCollection.deleteOne(query);
       res.send(result);
     })
 
